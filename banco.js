f9aa35231c2afe81003c577647908c552a6a1e0d
d4vwdi <d4vwdi@gmail.com>
2023-03-08 13:50:24 -0300

    criando o banco

--- banco.js
+++ banco.js
@@ -9,3 +9,50 @@ sequelize.authenticate().then(function(){
 }).catch(function(erro){
 	console.log("Falha ao conectar: " + erro)
 })
+
+
+const Agendamentos = sequelize.define("agendamentos",{
+	nome:{
+	type: Sequelize.STRING
+	},
+	endereco:{
+	type: Sequelize.STRING
+	},
+	bairro:{
+	type: Sequelize.STRING
+	},
+	cep:{
+	type: Sequelize.INTEGER
+	},
+	cidade:{
+	type: Sequelize.STRING
+	},
+	estado:{
+	type: Sequelize.STRING
+	},	
+	observacao:{
+	type: Sequelize.STRING
+	},
+})
+Agendamentos.sync({force:true})
+
+Agendamentos.create({
+	nome:"Jeferson Roberto de Lima",
+	endereco: "Av Águia de Haia",
+	bairro:"Jd São Nicolau",
+	cep:09239222,
+	cidade: "São Paulo",
+	estado:"SP",
+	observacao:"Teste para aula"
+})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
